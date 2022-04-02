e765e2c209287ff2a6162630dc08d648796dc1f1
nsingla20 <nsingla20@iitk.ac.in>
2022-04-02 08:46:55 +0530

    Revert "test2"
    This reverts commit c67b870738b064f15352aa3bb068d95987b47a4a.

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ BOOST_CLASS_EXPORT_GUID(Professor, "Professor")
 BOOST_CLASS_EXPORT_GUID(Librarian, "Librarian")
 using namespace std;
 
-int min(int, char**) {
+int main(int, char**) {
     UserDatabase::load();
     BookDatabase::load();
     atexit(UserDatabase::save);
